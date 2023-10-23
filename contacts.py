e12d23035feb04420050467a8a979f7860020145
Habib Amiri <amiri.habib@icloud.com>
2023-10-23 13:46:31 -0400

    blah

--- contacts.py
+++ contacts.py
@@ -1,20 +1,37 @@
 import json
+import re
 
 def contact():
-    try:
-        name = input("Enter the contacts name: ")
-        while True:
-            try: 
-                num = int(input("Enter number: "))
-                if isinstance(num, int) or len(num) > 10 or len(num) < 10:
-                    raise ValueError, "Enter a valid phone number"
-            except ValueError:
-                num = int(input("Enter number: "))
-            try:   
-                email = input("Enter email: ")
-                
+    contacts = []  
+    while True:
+        try:
+            name = input("Enter the contact's name: ")
+            num = input("Enter number: ")
+            if not num.isdigit() or len(num) != 10:
+                raise ValueError("Enter a valid phone number")
 
+            email = input("Enter email: ")
+            if not re.fullmatch(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", email):
+                raise ValueError("Make sure you enter a valid email.")
 
+            new_contact = {'name': name, 'number': num, 'email': email}
+            contacts.append(new_contact) 
+            print("Contact added successfully!")
 
+            with open('contacts.json', 'w') as f:
+                json.dump(contacts, f) 
 
-    
+            break  
+
+        except ValueError as ve:
+            print(ve)  
+
+        except:
+            print("Oops, something went wrong. Let's try again.")
+            continue 
+
+def main():
+    contact()
+
+if __name__ == "__main__":
+    main()
